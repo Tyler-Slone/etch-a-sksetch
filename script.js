43bc2a24f3f64f914068d636392257fd33c44476
Tyler Slone <tyl3rslon3@gmail.com>
2024-08-30 21:28:56 -0400

    Add grid size change function and add button functionality

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ let gridSize = 16;
 
 const setSize = document.querySelector("setSize");
 const grid = document.getElementById("grid");
+const changeSizeBtn = document.getElementById("changeSizeBtn");
 
+// Creates a 16x16 grid on page load and is mutable for changing grid size
 function createGrid(gridSize) {
   for (let i = 0; i < gridSize * gridSize; i++) {
     const gridElement = document.createElement("div");
@@ -11,6 +13,23 @@ function createGrid(gridSize) {
   }
 }
 
+//Dependency for changing grid size
+function clearGrid() {
+  grid.innerHTML = "";
+}
+
+function changeSize(gridSize) {
+  clearGrid();
+  createGrid(gridSize);
+}
+
+// On screen elements to load and interact with
 window.onload = () => {
   createGrid(gridSize);
 };
+
+//TODO Need to add error handling for inputs greater than 100, 0, negative, and not a num
+changeSizeBtn.addEventListener("click", () => {
+  gridSize = prompt("Please enter a new size up to 100");
+  changeSize(gridSize);
+});
